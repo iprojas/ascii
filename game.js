5759badd5e664d9378070cff62f484f7248902fd
iprojas <rojas.ip@gmail.com>
2021-03-15 14:11:22 -0300

    new

--- game.js
+++ game.js
@@ -70,10 +70,10 @@ var offsetY = 20;
 function getFlagInfo(level, z, x, y){
   if (level.map_id === INTRO) {
     if (x < 40) {
-      return {message: ["La segunda regla del desafío secreto es:", "Muevete con el teclado."],
+      return {message: ["Encuentra la presentacion de hoy.", "Muevete con el teclado."],
               finished: false};
     } else {
-      return {message:["Salta con ESPACIO", ""], finished: true };
+      return {message:["Felicitaciones!", "Navega a http://interactiva.xyz/clase1"], finished: false };
     }
   }
 
@@ -81,19 +81,19 @@ function getFlagInfo(level, z, x, y){
     if (Math.abs(x) < 100 && Math.abs(y) < 100) {
       let seed = Math.abs((x + y)%3);
       if (seed == 0) {
-        return {message: ["Acá no pasa nada"],
+        return {message: ["You'll have to look", "a little further", "than this!"],
               finished: false};
       }
       if (seed == 1) {
-        return {message: ["Esto es solo", "una distracción..."],
+        return {message: ["I hear you need", "to go far out..."],
               finished: false};
       }
       if (seed == 2) {
-        return {message: ["Tienes que buscar a otro nivel."],
+        return {message: ["Not here!"],
               finished: false};
       }
     } else {
-      return {message: ["Busca la fuente!"],
+      return {message: ["You made it!"],
               finished: true};
     }
   }
@@ -113,12 +113,12 @@ function getFlagInfo(level, z, x, y){
     let score = level.figureEightState[0] + level.figureEightState[1] + level.figureEightState[2];
 
     if (score >= 3) {
-      return {message:["Solo un paso más!", "( "+ score + " / 3)"], finished:true};
+      return {message:["You made it!", "( "+ score + " / 3)"], finished:true};
     }
 
-    return {message:["Un poco más!", "(" + score + " / 3)"], finished:false};
+    return {message:["Keep going!", "(" + score + " / 3)"], finished:false};
   }
-  return {message:["Felicitaciones, no ganaste nada!"], finished:true}
+  return {message:["Finish!"], finished:true}
 };
 
 // Modify buffer sizes and change rendering parameters according to the provided dimensions.
